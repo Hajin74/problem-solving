d4fc370875d0198bf44f06adb508224e37da2fe7
Hajin <67518596+Hajin74@users.noreply.github.com>
2023-02-27 19:47:24 +0900

    [Silver IV] Title: 큐, Time: 76 ms, Memory: 34176 KB -BaekjoonHub

---
+++ 백준/Silver/10845. 큐/큐.py
@@ -0,0 +1,33 @@
+from collections import deque
+import sys
+
+q = deque()
+n = int(sys.stdin.readline())
+
+for i in range(n):
+    order = list(map(str, sys.stdin.readline().split()))
+
+    if order[0] == 'push':
+        q.append(order[1])
+    elif order[0] == 'pop':
+        if len(q) > 0:
+            print(q.popleft())
+        else:
+            print(-1)
+    elif order[0] == 'size':
+        print(len(q))
+    elif order[0] == 'empty':
+        if len(q) == 0:
+            print(1)
+        else:
+            print(0)
+    elif order[0] == 'front':
+        if len(q) > 0:
+            print(q[0])
+        else:
+            print(-1)
+    elif order[0] == 'back':
+        if len(q) > 0:
+            print(q[-1])
+        else:
+            print(-1)
