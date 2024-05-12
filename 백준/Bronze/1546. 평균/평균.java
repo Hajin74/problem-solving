fd02191361784398c1a568bd5d7de36639350fb0
Hajin <67518596+Hajin74@users.noreply.github.com>
2024-05-13 08:09:58 +0900

    [Bronze I] Title: 평균, Time: 256 ms, Memory: 18776 KB -BaekjoonHub

---
+++ 백준/Bronze/1546. 평균/평균.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        long sum = 0;
+        long max = 0;
+
+        for (int i = 0; i < n; i++) {
+            int temp = sc.nextInt();
+            if (temp > max) {
+                max = temp;
+            }
+            sum += temp;
+        }
+
+        double result = sum * 100.0 / max / n;
+
+        System.out.println(result);
+    }
+}
