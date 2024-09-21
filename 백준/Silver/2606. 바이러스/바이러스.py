2fc7ea0b5e6ac4a2c81166d136349df3e92bdc59
Hajin <67518596+Hajin74@users.noreply.github.com>
2024-09-21 14:58:37 +0900

    [Silver III] Title: 바이러스, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.py
+++ 백준/Silver/2606. 바이러스/바이러스.py
@@ -1,20 +1,28 @@
-computer = int(input())
-connected_pair = int(input())
+# from collections import deque
+import sys
 
-graph = [[] for _ in range(computer + 1)]
-for i in range(connected_pair):
-  n, m = map(int, input().split())
-  graph[n].append(m)
-  graph[m].append(n)
+n = int(input())  # 컴퓨터 수
+m = int(input())  # 네트워크 수
+graph = [[] for _ in range(n+1)] # 인접 리스트
+
+# 네트워크 입력받기
+for i in range(m):
+    a, b = map(int, sys.stdin.readline().split())
+    graph[a].append(b)
+    graph[b].append(a)
+# print(graph)
+
+visited = [False] * (n+1)
 
 def dfs(graph, v, visited):
-  visited[v] = True
-  for i in graph[v]:
-    if visited[i] == False:
-      dfs(graph, i, visited)
+    visited[v] = True
+
+    for i in graph[v]:
+        if not visited[i]:
+            dfs(graph, i, visited)
 
 
-visited = [False] * (computer + 1)
 dfs(graph, 1, visited)
 
+# 본 컴퓨터 빼고 바이러스에 걸리게 되는 컴퓨터 수 출력   
 print(visited.count(True) - 1)
