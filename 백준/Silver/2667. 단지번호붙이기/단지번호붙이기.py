1897dcfdb5d126b434dd59a9c1a13a826583c156
Hajin <67518596+Hajin74@users.noreply.github.com>
2024-09-21 15:34:11 +0900

    [Silver I] Title: 단지번호붙이기, Time: 52 ms, Memory: 34088 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
@@ -1,39 +1,48 @@
-N = int(input())
+from collections import deque
 
+n = int(input()) # 지도의 크기
 graph = []
-for i in range(N):
-  graph.append(list(map(int, input())))
+for i in range(n):
+    graph.append(list(map(int, input())))
 
-count = 0
-def dfs(x, y):
-  if x < 0 or x >= N or y < 0 or y >= N:
-    return False
+# 방향
+dx = [-1, 1, 0, 0]
+dy = [0, 0, -1, 1]
 
-  if graph[x][y] == 1:
-    global count
-    count += 1
-    
+def bfs(x, y):
+    queue = deque()
+    queue.append([x, y])
+
+    # 첫번째 집 방문 처리
     graph[x][y] = 0
-    dfs(x - 1, y)
-    dfs(x + 1, y)
-    dfs(x, y - 1)
-    dfs(x, y + 1)
+    count = 1 
 
-    return True
+    while queue:
+        x, y = queue.popleft()
 
-  return False
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
 
+            if nx < 0 or ny < 0 or nx >= n or ny >= n:
+                continue
 
-list = []
-result = 0
-for i in range(N):
-  for j in range(N):
-    if dfs(i, j) == True:
-      list.append(count)
-      count = 0
-      result += 1
+            if graph[nx][ny] == 1:
+                graph[nx][ny] = 0
+                queue.append([nx, ny])
+                count += 1
+    
+    return count
 
-print(result)
+result = []
 
-for item in sorted(list):
-  print(item)
+for i in range(n):
+    for j in range(n):
+        if graph[i][j] == 1:
+            count = bfs(i, j)
+            result.append(count)
+result.sort()
+
+print(len(result))
+for k in result:
+    print(k)
