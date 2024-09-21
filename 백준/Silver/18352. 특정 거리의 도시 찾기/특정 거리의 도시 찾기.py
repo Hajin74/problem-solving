10fa59710fd9e262c95999eb15a747be3f305ee1
Hajin <67518596+Hajin74@users.noreply.github.com>
2024-09-21 13:41:22 +0900

    [Silver II] Title: 특정 거리의 도시 찾기, Time: 1756 ms, Memory: 99940 KB -BaekjoonHub

--- 백준/Silver/18352. 특정 거리의 도시 찾기/특정 거리의 도시 찾기.py
+++ 백준/Silver/18352. 특정 거리의 도시 찾기/특정 거리의 도시 찾기.py
@@ -1,29 +1,30 @@
 from collections import deque
 import sys
-input = sys.stdin.readline
 
-n, m, k, x = map(int, input().split())
-graph = [[] for _ in range(n + 1)]
+n, m, k, x = map(int, input().split()) # 도시, 도로, 최단거리, 출발도시
+graph = [[] for _ in range(n+1)] # 도시만큼 인접 리스트 만들기
 
-for _ in range(m):
-    a, b = map(int, input().split())
+# 해당하는 도시에 도로 추가하기
+for i in range(m):
+    a, b = map(int, sys.stdin.readline().split())
     graph[a].append(b)
 
-distance = [-1] * (n + 1)
-distance[x] = 0 
+# 거리 기록하기
+distance = [-1] * (n+1)
+distance[x] = 0 # 출발지는 거리 0으로 초기화
 
+queue = deque([x])
+while queue:
+    now = queue.popleft() 
 
-q = deque([x])
-while q:
-    now = q.popleft()
-    
-    for next_node in graph[now]:
-        if distance[next_node] == -1:
-            distance[next_node] = distance[now] + 1
-            q.append(next_node)
+    # 현재 노드에서 인접한 노드 방문해서
+    for i in graph[now]:
+        if distance[i] == -1: # 처음 방문한 노드이면
+            distance[i] = distance[now] + 1 # 최단 거리 기록
+            queue.append(i)
 
 check = False
-for i in range(1, n + 1):
+for i in range(1, n+1):
     if distance[i] == k:
         print(i)
         check = True
