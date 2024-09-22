4b89311f633f5ea6d0eaa2be2402c12c7a6d13d3
Hajin <67518596+Hajin74@users.noreply.github.com>
2024-09-22 19:35:48 +0900

    [Bronze II] Title: 럭키 스트레이트, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/18406. 럭키 스트레이트/럭키 스트레이트.py
+++ 백준/Bronze/18406. 럭키 스트레이트/럭키 스트레이트.py
@@ -1,24 +1,13 @@
-n = int(input())
-s = str(n)
+arr = input()
+left = 0
+right = 0
 
-# 자릿수 구하기
-count = 1 
-while n >= 10:
-    count += 1
-    n //= 10
+middle_index = len(arr) // 2
+for i in range(middle_index):
+    left += int(arr[i])
+    right += int(arr[middle_index + i])
 
-middle_index = int(count / 2)
-
-# 반으로 나누기
-s_list = list(s)
-left_list = list(map(int, s_list[0:middle_index]))
-right_list = list(map(int, s_list[middle_index:]))
-
-# 자릿수 합 구하기
-left_sum = sum(left_list)
-right_sum = sum(right_list)
-
-if left_sum == right_sum:
-    print("LUCKY")
+if left == right:
+    print('LUCKY')
 else:
-    print("READY")
+    print('READY')
