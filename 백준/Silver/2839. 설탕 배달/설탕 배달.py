7fe8550cd497d53e688d259bddb192191e944278
Hajin <67518596+Hajin74@users.noreply.github.com>
2024-09-25 01:21:22 +0900

    [Silver IV] Title: 설탕 배달, Time: 36 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/2839. 설탕 배달/설탕 배달.py
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.py
@@ -1,13 +1,17 @@
-n = int(input())
+kg = int(input())
 
-result = 0
-while n >= 0:
-  if n % 5 == 0:
-    result += n // 5
-    print(result)
-    break
-  n -= 3
-  result += 1
+# kg가 5로 나눠질 경우
+# kg가 3과 5로 나눠질 경우
+# kg가 3으로 나눠질 경우
+# kg가 3과 5로 나눠지지 않을 경우
+
+bag = 0
+while kg >= 0:
+    if kg % 5 == 0: # 5의 배수이면
+        bag += kg // 5 # 몫 바로 출력
+        print(bag) 
+        break
+    kg -= 3 # 5의 배수가 될 때까지, 설탕 -3 봉지 +1
+    bag += 1 
 else:
-  print(-1)
-    
+    print(-1)
