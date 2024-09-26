dc684b2dd03a14b1ebde6a60341a9a973135758f
Hajin <67518596+Hajin74@users.noreply.github.com>
2024-09-26 11:53:41 +0900

    [Silver IV] Title: 스택, Time: 500 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/10828. 스택/스택.py
+++ 백준/Silver/10828. 스택/스택.py
@@ -1,50 +1,25 @@
-import sys
-
 n = int(input())
+
 stack = []
-
-
-def push(num):
-    stack.append(num)
-
-
-def pop():
-    if len(stack) > 0:
-        result = stack.pop()
-    else:
-        result = -1
-
-    print(result)
-
-
-def size():
-    print(len(stack))
-
-
-def empty():
-    if len(stack) == 0:
-        print(1)
-    else:
-        print(0)
-
-
-def top():
-    if len(stack) == 0:
-        print(-1)
-    else:
-        print(stack[-1])
-
-
 for i in range(n):
-    order = list(map(str, sys.stdin.readline().split()))
-    # print(order)
-    if order[0] == 'push':
-        push(int(order[1]))
-    elif order[0] == 'top':
-        top()
-    elif order[0] == 'size':
-        size()
-    elif order[0] == 'empty':
-        empty()
-    elif order[0] == 'pop':
-        pop()
+    cmd = input().split()
+    if cmd[0] == 'push':
+        stack.append(cmd[1])
+    elif cmd[0] == 'pop':
+        if len(stack) == 0:
+            print(-1)
+        else:
+            c = stack.pop()
+            print(c)
+    elif cmd[0] == 'size':
+        print(len(stack))
+    elif cmd[0] == 'empty':
+        if len(stack) == 0:
+            print(1)
+        else:
+            print(0)
+    elif cmd[0] == 'top':
+        if len(stack) == 0:
+            print(-1)
+        else:
+            print(stack[-1])
