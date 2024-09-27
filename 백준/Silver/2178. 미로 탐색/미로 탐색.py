909f0445d752af854a618e7026160b693d89c734
Hajin <67518596+Hajin74@users.noreply.github.com>
2024-09-27 17:49:34 +0900

    [Silver I] Title: 미로 탐색, Time: 60 ms, Memory: 34072 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.py
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.py
@@ -1,37 +1,39 @@
 from collections import deque
-import sys
 
-# 입력 받기
-n, m = map(int, input().split())
+N, M = map(int, input().split())
+
 graph = []
-for i in range(n):
-    graph.append(list(map(int, input())))
+for _ in range(N):
+     graph.append(list(map(int, input())))
 # print(graph)
 
-# 방향
 dx = [-1, 1, 0, 0]
 dy = [0, 0, -1, 1]
 
-def bfs(x, y):
-    queue = deque()
-    queue.append((x, y))
 
+# 방문하지 않은거 큐에 넣고 방문처리
+
+def bfs(n, m):
+    queue = deque()
+    queue.append((n, m))
+
+    graph[n][m] = 1 # 현재 방문 처리
+
+    # 큐가 빌 때까지
     while queue:
         x, y = queue.popleft()
 
+        # 주변 탐색
         for i in range(4):
-            nx = x + dx[i]
-            ny = y + dy[i]
-   
+            nx, ny = x + dx[i], y+ dy[i]
+
             # 범위 제한하기
-            if nx < 0 or ny < 0 or nx >= n or ny >= m:
+            if nx < 0 or ny < 0 or nx >= N or ny >= M:
                 continue
 
-            # 처음 방문한 노드면 최단 거리 기록
             if graph[nx][ny] == 1:
                 graph[nx][ny] = graph[x][y] + 1
                 queue.append((nx, ny))
+bfs(0, 0)
+print(graph[N-1][M-1])
 
-    return graph[n-1][m-1]
-
-print(bfs(0, 0))
