99808af011dd729eebe4ec3aed1a2799b89ca640
Hajin <67518596+Hajin74@users.noreply.github.com>
2024-09-27 16:14:15 +0900

    [Silver V] Title: 단어 정렬, Time: 524 ms, Memory: 36396 KB -BaekjoonHub

--- 백준/Silver/1181. 단어 정렬/단어 정렬.py
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.py
@@ -1,13 +1,7 @@
-import sys
-
-# 첫 번째 난관 : 문자열 길이대로 정렬은 했으나 동일할 때 알파벳 순서대로 정렬 못함 -> 처음부터 알파벳순으로 정렬
-
 n = int(input())
-words = [sys.stdin.readline().rstrip() for _ in range(n)]
-words.sort()
-lengths = [len(x) for x in words]
-word_dict = dict(zip(words, lengths))
 
-sorted_words = sorted(word_dict, key=lambda x: word_dict[x])
-for word in sorted_words:
+words = set([input() for _ in range(n)])
+words = sorted(words, key=lambda x : (len(x), x))
+
+for word in words:
     print(word)
