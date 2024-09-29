68d0369f7f08a8c0e0aa6a8c428ad70675cc5ba8
Hajin <67518596+Hajin74@users.noreply.github.com>
2024-09-29 16:43:39 +0900

    [Silver II] Title: 촌수계산, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/2644. 촌수계산/촌수계산.py
+++ 백준/Silver/2644. 촌수계산/촌수계산.py
@@ -1,8 +1,4 @@
-# 주변 노드들을 탐색해서 몇 번의 이동으로 도착했는지 기록
-# bfs로 주변에 있는 것부터 기록
-# 해당하는 도드의 이동횟수 출력
-
-from collections import deque
+# dfs로도 풀어보자
 
 n = int(input()) # 사람 수
 a, b = map(int, input().split()) # 촌수를 알고 싶은 사람 번호
@@ -16,21 +12,15 @@ for _ in range(k):
     graph[j].append(i)
 #print(graph)
 
-def bfs(graph, v, depth):
-    queue = deque()
-    queue.append(v)
-    depth[v] += 1 # 출발지 0으로 초기화
-
-    while queue:
-        now = queue.popleft()
-
-        for i in graph[now]:
-            # 방문 안했으면 전에 있던 곳으로 부터 +1
-            if depth[i] == -1:
-                depth[i] = depth[now] + 1
-                queue.append(i)
+def dfs(graph, v, depth):
+    for i in graph[v]:
+        if depth[i] == -1:
+            depth[i] = depth[v] + 1
+            dfs(graph, i, depth)
 
 # 출발지 x = 0, 출발지로 부터 방문 기록 +1
 depth = [-1 for _ in range(n+1)]
-bfs(graph, a, depth)
+depth[a] += 1
+dfs(graph, a, depth)
+# print(depth)
 print(depth[b])
