ffc5ade4c896996078b4a2557954b81e03680160
Hajin <67518596+Hajin74@users.noreply.github.com>
2024-09-29 15:53:41 +0900

    [Silver II] Title: 유기농 배추, Time: 208 ms, Memory: 34088 KB -BaekjoonHub

--- 백준/Silver/1012. 유기농 배추/유기농 배추.py
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.py
@@ -1,39 +1,46 @@
-import sys
-
-sys.setrecursionlimit(10**7)
-
-
-def dfs(n, m):
-  if n < 0 or n >= N or m < 0 or m >= M:
-    return False
-
-  if graph[n][m] == 1:
-    graph[n][m] = 0
-    dfs(n - 1, m)
-    dfs(n + 1, m)
-    dfs(n, m - 1)
-    dfs(n, m + 1)
-    return True
-
-  return False
-
+# 이것도 최단경로를 구하는 것이 아니라, 덩어리만 구하는 것으로 dfs/bfs 둘 다 가능
+from collections import deque
 
 T = int(input())
 
-result = [0] * T
-for t in range(T):
-  M, N, Y = map(int, input().split())
+# 방향
+dx = [-1, 1, 0, 0]
+dy = [0, 0, -1, 1]
 
-  graph = [[0 for _ in range(M)] for _ in range(N)]
-  for _ in range(Y):
-    m, n = map(int, input().split())
-    graph[n][m] = 1
+def bfs(x, y):
+    queue = deque()
+    queue.append((x, y))
+    graph[x][y] = 0 # 현재 방문 처리
 
-  result[t] = 0
-  for n in range(N):
-    for m in range(M):
-      if dfs(n, m):
-        result[t] += 1
+    while queue:
+        now_x, now_y = queue.popleft()
 
-for r in result:
-  print(r)
+        for i in range(4):
+            nx, ny = now_x + dx[i], now_y + dy[i]
+
+            if nx < 0 or ny < 0 or nx >= n or ny >= m:
+                continue
+
+            if graph[nx][ny] == 1:
+                graph[nx][ny] = 0
+                queue.append((nx, ny))
+
+
+for _ in range(T):
+    n, m, k = map(int, input().split())
+    graph = [[0] * m for _ in range(n)]
+
+    # 배추 위치 받기
+    for _ in range(k):
+        i, j = map(int, input().split())
+        graph[i][j] = 1
+    # print(graph)
+
+    count = 0
+    for i in range(n):
+        for j in range(m):
+            if graph[i][j] == 1:
+                bfs(i, j)
+                count += 1
+
+    print(count)
