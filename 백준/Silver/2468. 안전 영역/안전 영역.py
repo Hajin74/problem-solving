e16c0314e63d3edd8882ac310d64ee5c648ca1c5
Hajin <67518596+Hajin74@users.noreply.github.com>
2024-09-29 22:22:23 +0900

    [Silver I] Title: 안전 영역, Time: 1276 ms, Memory: 33140 KB -BaekjoonHub

--- 백준/Silver/2468. 안전 영역/안전 영역.py
+++ 백준/Silver/2468. 안전 영역/안전 영역.py
@@ -1,36 +1,48 @@
 import sys
-sys.setrecursionlimit(100000)
-r = sys.stdin.readline
+import copy
 
-# 상 우 하 좌
-dx = [-1, 0, 1, 0]
-dy = [0, 1, 0, -1]
+sys.setrecursionlimit(10**6)
+n  = int(input())
+graph = [list(map(int, sys.stdin.readline().split())) for _ in range(n)]
+
+# 방향
+dx = [-1, 1, 0, 0]
+dy = [0, 0, -1, 1]
+
+def dfs(graph, x, y):
+    graph[x][y] = 0
+
+    for i in range(4):
+        nx, ny = x + dx[i], y + dy[i]
+
+        if nx < 0 or ny < 0 or nx >= n or ny >= n:
+                continue
+
+        if graph[nx][ny] != 0:
+            dfs(graph, nx, ny)
 
 
-def dfs(x, y, h):
+# 비의 잠긴 부분 표시하기
+def under_water(height):
+    under_graph = copy.deepcopy(graph)
+    for i in range(n):
+        for j in range(n):
+            if under_graph[i][j] <= height:
+                under_graph[i][j] = 0 # 잠김 처리
+    return under_graph
 
-    for m in range(4):
-        nx = x + dx[m]
-        ny = y + dy[m]
+# 최대 영역 찾기
+max_height = max(map(max, graph))
 
-        if (0 <= nx < N) and (0 <= ny < N) and not visit[nx][ny] and zone[nx][ny] > h:
-            visit[nx][ny] = True
-            dfs(nx, ny, h)
+areas = []
+for k in range(max_height + 1):
+    under_graph = under_water(k)
+    count = 0
+    for i in range(n):
+        for j in range(n):
+            if under_graph[i][j] != 0:
+                dfs(under_graph, i, j)
+                count += 1
+    areas.append(count)
 
-
-N = int(r())
-zone = [list(map(int, r().split())) for _ in range(N)]
-
-ans = 1
-for k in range(max(map(max, zone))):
-    visit = [[False]*N for _ in range(N)]
-    safe = 0
-    for i in range(N):
-        for j in range(N):
-            if zone[i][j] > k and not visit[i][j]:
-                safe += 1
-                visit[i][j] = True
-                dfs(i, j, k)
-    ans = max(ans, safe)
-
-print(ans)
+print(max(areas))
