e57a24b8d7a705ab82e279c4ecc89d89667494f8
Hajin <67518596+Hajin74@users.noreply.github.com>
2024-09-29 21:13:52 +0900

    [Silver II] Title: 연결 요소의 개수, Time: 568 ms, Memory: 65048 KB -BaekjoonHub

--- 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.py
+++ 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.py
@@ -1,30 +1,31 @@
+from collections import deque
 import sys
 
-sys.setrecursionlimit(10**7)
+n, m = map(int, sys.stdin.readline().split())
 
-N, M = map(int, input().split())
-graph = [[] for _ in range(N + 1)]
-for _ in range(M):
-  u, v = map(int, sys.stdin.readline().split())
-  graph[u].append(v)
-  graph[v].append(u)
+graph = [[] for _ in range(n+1)]
+for _ in range(m):
+    u, v = map(int, sys.stdin.readline().split())
+    graph[u].append(v)
+    graph[v].append(u)
 
 
-def dfs(graph, v, visited):
-  if visited[v]:
-    return False
+def bfs(graph, v, visited):
+    queue = deque([v])
+    visited[v] = True
 
-  visited[v] = True
-  for i in graph[v]:
+    while queue:
+        curr_node = queue.popleft()
+        for next_node in graph[curr_node]:
+            if not visited[next_node]:
+                visited[next_node] = True
+                queue.append(next_node)
+
+visited = [False for _ in range(n+1)]
+count = 0
+for i in range(1, n+1):
     if not visited[i]:
-      dfs(graph, i, visited)
-  return True
+        bfs(graph, i, visited)
+        count += 1
 
-
-result = 0
-visited = [False] * (N + 1)
-for i in range(1, N + 1):
-  if dfs(graph, i, visited):
-    result += 1
-
-print(result)
+print(count)
