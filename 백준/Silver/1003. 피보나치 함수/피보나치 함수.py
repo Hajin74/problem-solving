c90916ed652930e80187fa31245191974ccd545e
Hajin <67518596+Hajin74@users.noreply.github.com>
2024-09-30 17:44:34 +0900

    [Silver III] Title: 피보나치 함수, Time: 36 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1003. 피보나치 함수/피보나치 함수.py
+++ 백준/Silver/1003. 피보나치 함수/피보나치 함수.py
@@ -1,16 +1,16 @@
-def calc(n):
-    zero = [1, 0, 1]
-    one = [0, 1, 1]
-
-    if len(zero) <= n:
-        for i in range(len(zero), n + 1):
-            zero.append(zero[i-2] + zero[i-1])
-            one.append(one[i-2] + one[i-1])
-    
-    return zero[n], one[n]
-
 t = int(input())
-for i in range(t):
+
+for _ in range(t):
     n = int(input())
-    zero, one = calc(n)
-    print(zero, one)
+
+    zero = [0] * 41
+    one = [0] * 41
+
+    zero[0], zero[1] = 1, 0
+    one[0], one[1] = 0, 1
+
+    for i in range(2, n+1):
+        zero[i] = zero[i-1] + zero[i-2]
+        one[i] = one[i-1] + one[i-2]
+
+    print(zero[n], one[n])
