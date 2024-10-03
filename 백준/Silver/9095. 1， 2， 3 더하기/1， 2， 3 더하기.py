7b83eb55eac015acfef1ce407a14e2fb630d9326
Hajin <67518596+Hajin74@users.noreply.github.com>
2024-10-03 13:56:34 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.py
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.py
@@ -1,17 +1,16 @@
-t = int(input())
+# f(i) = f(i-1) + f(i-2) + f(i-3)
 
-for _ in range(t):
+T = int(input())
+
+dp = [0] * 12
+dp[1] = 1
+dp[2] = 2
+dp[3] = 4
+
+for t in range(T):
     n = int(input())
-    dp = [0] * (n+1)
 
-    for i in range(1, n+1):
-        if i == 1:
-            dp[i] = 1
-        elif i == 2:
-            dp[i] = 2
-        elif i == 3:
-            dp[i] = 4
-        else:
-            dp[i] = dp[i-1] + dp[i-2] + dp[i-3]
+    for i in range(4, n+1):
+        dp[i] = dp[i-1] + dp[i-2] + dp[i-3]
 
     print(dp[n])
