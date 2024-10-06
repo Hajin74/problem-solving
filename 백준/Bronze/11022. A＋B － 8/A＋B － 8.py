2b45154f9717626ee78b34a3bce06c8774479354
Hajin <67518596+Hajin74@users.noreply.github.com>
2024-10-06 13:12:43 +0900

    [Bronze V] Title: A+B - 8, Time: 36 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/11022. A＋B － 8/A＋B － 8.py
+++ 백준/Bronze/11022. A＋B － 8/A＋B － 8.py
@@ -1,6 +1,6 @@
-class Solution :
-    t = int(input())
-    for i in range(t) :
-        a, b = map(int, input().split())
-        print('Case #%d: %d + %d = %d' % (i + 1, a, b, a + b))
-    
+t = int(input())
+for i in range(t):
+    a, b = map(int, input().split())
+    result = a + b
+    index = i + 1
+    print(f"Case #{index}: {a} + {b} = {result}")
