ec1ea1fb78f6bb69925755157aaaad18e6cc7fa2
Hajin <67518596+Hajin74@users.noreply.github.com>
2024-10-07 22:08:42 +0900

    [Bronze I] Title: 수 정렬하기 3, Time: 7860 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.py
+++ 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.py
@@ -1,12 +1,17 @@
 import sys
 
 n = int(sys.stdin.readline())
-nums = [0] * 10001
+
+count = [0] * 10001 # 입력받을 수의 크기만큼
+
+# 1은 몇 개, 2는 몇 개 이런식으로 기록
 
 for _ in range(n):
-    nums[int(sys.stdin.readline())] += 1
+    num = int(sys.stdin.readline())
+    count[num] += 1
 
-for i in range(10001):
-    if nums[i] != 0:
-        for j in range(nums[i]):
+for i in range(1, 10001):
+    # 기록된 만큼 출력하자
+    if count[i] != 0:
+        for _ in range(count[i]):
             print(i)
