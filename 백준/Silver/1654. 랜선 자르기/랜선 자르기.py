8fe4ddda8f4c3f21b82e794677eafcdede5d6584
Hajin <67518596+Hajin74@users.noreply.github.com>
2024-10-07 23:50:40 +0900

    [Silver II] Title: 랜선 자르기, Time: 300 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1654. 랜선 자르기/랜선 자르기.py
+++ 백준/Silver/1654. 랜선 자르기/랜선 자르기.py
@@ -1,23 +1,21 @@
-n, m = map(int, input().split())
-list = []
-for _ in range(n):
-  list.append(int(input()))
+k, n = map(int, input().split()) # 이미 가지고 있는 랜선 개수, 필요한 랜선 개수
+lans = [int(input()) for _ in range(k)]
 
 start = 1
-end = max(list)
+end = max(lans)
 
-result = 0
 while start <= end:
-  total = 0
-  mid = (start + end) // 2
-    
-  for item in list:
-    total += item // mid
-    
-  if total < m:
-    end = mid - 1
-  else:
-    result = mid
-    start = mid + 1
-    
-print(result)
+    mid = (start + end) // 2
+
+    # 랜선 자르기
+    count = 0
+    for lan in lans:
+        count += lan // mid
+    # print("count:", count, "size:", mid)
+
+    if count >= n:
+        start = mid + 1
+    else:
+        end = mid - 1
+
+print(end)
