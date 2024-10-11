94a1949d8cb594714d736bbbeb11f08a61a0e0fc
Hajin <67518596+Hajin74@users.noreply.github.com>
2024-10-11 16:55:21 +0900

    [Silver II] Title: 예산, Time: 60 ms, Memory: 32140 KB -BaekjoonHub

--- 백준/Silver/2512. 예산/예산.py
+++ 백준/Silver/2512. 예산/예산.py
@@ -1,33 +1,29 @@
-N = int(input())
-array = list(map(int, input().split()))
-# cut = array.copy()
-budget = int(input())
+# 나눠줄 수 있는 최대의 예산을 찾아라
+# 1 <= 최대의 예산 <= 최대의 요청예산
 
-if sum(array) <= budget:
-  print(max(array))
-else:
-  start = 1
-  end = max(array)
+n = int(input()) # 지방 수
+requests = list(map(int, input().split())) # 각 지방의 요청예산들
+requests.sort()
+total_budget = int(input()) # 총 예산
 
-  result = 0
-  while start <= end:
+start = 1
+end = requests[-1]
+max_budget = 1
+
+while start <= end:
     mid = (start + end) // 2
-    total = 0
 
-    cut = []
-    for item in array:
-      if item > mid:
-        cut.append(mid)
-        total += mid
-      else:
-        cut.append(item)
-        total += item
-      
-    if total > budget:
-      end = mid - 1
-    else:
-      result = max(cut)
-      start = mid + 1
-      
-  print(result)
+    sum = 0
+    for request in requests:
+        if mid < request:
+            sum += mid
+        else:
+            sum += request
     
+    if sum <= total_budget:
+        max_budget = max(max_budget, mid)
+        start = mid + 1
+    else:
+        end = mid - 1
+
+print(max_budget)
