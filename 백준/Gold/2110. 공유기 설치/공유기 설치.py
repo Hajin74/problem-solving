eb2c12caec4cd2df680616579f864521085c060d
Hajin <67518596+Hajin74@users.noreply.github.com>
2024-10-11 16:20:47 +0900

    [Gold IV] Title: 공유기 설치, Time: 5080 ms, Memory: 38848 KB -BaekjoonHub

--- 백준/Gold/2110. 공유기 설치/공유기 설치.py
+++ 백준/Gold/2110. 공유기 설치/공유기 설치.py
@@ -1,29 +1,30 @@
+# 정해진 공유기를 최대한 인접하지 않게 멀리멀리 설치하기
+
 n, c = map(int, input().split())
-homes = []
-for _ in range(n):
-  homes.append(int(input()))
+homes = [int(input()) for _ in range(n)]
 homes.sort()
 
+# 최소거리, 최대거리
 start = 1
 end = homes[-1] - homes[0]
-result = 0
 
 while start <= end:
-  mid = (start + end) // 2
-  current = homes[0]
-  count = 1
+    mid = (start + end) // 2
 
-  # 현재 위치의 집과 해당 집의 거리가 최소 거리를 지키는가
-  for i in range(1, n):
-    if homes[i] >= current + mid:
-      count += 1
-      current = homes[i]
+    current_home = homes[0]
+    count = 1
 
-  if count >= c:
-    result = mid
-    start = mid + 1
-  else:
-    end = mid - 1
+    # 거리만 만족하면 더 넓히지 않고 카운팅
+    for i in range(1, n):
+        if homes[i] >= current_home + mid:
+            count += 1
+            current_home = homes[i]
+    
+    # 공유기가 다 설치될만큼의 거리이면, 거리를 더 넓힐 수 있는지 시도
+    if count >= c:
+        start = mid + 1
+        result = mid # 현재 결과를 저장하자, 더 넓히지 못할 수도 있으니까, 이게 최선의 답일 수 있다
+    else:
+        end = mid - 1
 
 print(result)
-    
