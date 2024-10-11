0637b9e99668e68a937097703559f90968d36404
Hajin <67518596+Hajin74@users.noreply.github.com>
2024-10-11 15:37:37 +0900

    [Silver II] Title: 나무 자르기, Time: 4436 ms, Memory: 143316 KB -BaekjoonHub

--- 백준/Silver/2805. 나무 자르기/나무 자르기.py
+++ 백준/Silver/2805. 나무 자르기/나무 자르기.py
@@ -1,20 +1,28 @@
-n, m = map(int, input().split())
-list = list(map(int, input().split()))
+# 0 <= h <= 1000000000
+# 필요한 m미터 나무를 가져가기 위해 절단기에 설정할 최대의 높이 h를 구하라
+# 적절한 h를 선택하는 것이 핵심
+# 1부터 최대의 나무 높이까지 탐색하면서 찾아도되지만 시간이 오래걸리니 이분탐색으로 찾아보자
+
+n, m = map(int, input().split()) # 주어진 나무 수, 필요한 나무 길이
+trees = list(map(int, input().split()))
 
 start = 0
-end = max(list)
+end = max(trees)
 
-result = 0
 while start <= end:
-  total = 0
-  mid = (start + end) // 2
-  for tree in list:
-    if tree > mid:
-      total += tree - mid
-  if total < m :
-    end = mid - 1
-  else:
-    result = mid
-    start = mid + 1
+    mid = (start + end) // 2
 
-print(result)
+    meter = 0
+    for tree in trees:
+        # 자를 수 있는 높이가 되는 나무만 자르기
+        if tree >= mid:
+            meter = meter + (tree - mid)
+    
+    # 나무가 부족하면 더 낮게 잘라야해
+    if meter < m:
+        end = mid - 1
+    # 나무가 충분하면 더 높은 곳에서 자를 있는지 시도
+    else:
+        start = mid + 1
+
+print(end)
