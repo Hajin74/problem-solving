12ec69fec0b532572514876edac5c73159ae49cd
Hajin <67518596+Hajin74@users.noreply.github.com>
2024-10-13 01:17:17 +0900

    [level 2] Title: 기능개발, Time: 0.56 ms, Memory: 79.1 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.java
+++ 프로그래머스/2/42586. 기능개발/기능개발.java
@@ -1,29 +1,27 @@
-import java.util.ArrayList; 
-import java.util.Stack;
+import java.util.*;
 
 class Solution {
-    public int[] solution(int[] progresses, int[] speeds) { 
-        ArrayList<Integer> result = new ArrayList<>();
-        Stack<Integer> stack = new Stack<>();
-
+    public ArrayList<Integer> solution(int[] progresses, int[] speeds) {
+        ArrayList<Integer> answer = new ArrayList<>();
+        Queue<Integer> queue = new LinkedList<>();
+        
+        // 작업 일수 계산
         for (int i = 0; i < progresses.length; i++) {
-            int days =  (100 - progresses[i]) / speeds[i];
-            int remain =  (100 - progresses[i]) % speeds[i];
-            if (remain > 0) {days++;}
-
-            if (i > 0) {
-                if (stack.get(0) < days) {
-                    result.add(stack.size());
-                    stack.clear();
-                }
-            }
-            stack.push(days);
+            // 나머지가 있으면 올림
+            int day = (int)(Math.ceil((100.0 - progresses[i]) / speeds[i]));
+            queue.add(day);
         }
-        result.add(stack.size());
-
-        int[] answer = new int[result.size()];
-        for (int i = 0; i < result.size(); i++) {
-            answer[i] = result.get(i);
+        
+        while (!queue.isEmpty()) {
+            int days = queue.poll();
+            int count = 1;
+            
+            while(!queue.isEmpty() && queue.peek() <= days) {
+                queue.poll();
+                count++;
+            }
+            
+            answer.add(count);
         }
         
         return answer;
