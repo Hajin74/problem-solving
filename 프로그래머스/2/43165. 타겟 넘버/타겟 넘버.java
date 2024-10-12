35e666674d50418e31a36f8b51cec527c1bee787
Hajin <67518596+Hajin74@users.noreply.github.com>
2024-10-13 00:00:19 +0900

    [level 2] Title: 타겟 넘버, Time: 211.14 ms, Memory: 161 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
@@ -1,27 +1,29 @@
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 class Solution {
     public int solution(int[] numbers, int target) {
-        List<Integer> leaves = new ArrayList<>();
-        leaves.add(0);
-        
-        for (Integer num : numbers) {
-            List<Integer> temp = new ArrayList<>();
-            for (Integer leaf : leaves) {
-                temp.add(leaf + num);
-                temp.add(leaf - num);
-            }
-            leaves = temp;
-        }
-        
         int answer = 0;
-        for (Integer leaf : leaves) {
-            if (leaf == target) {
-                answer++;
-            }
-        }
         
+        // bfs 돌면서 연산 결과를 저장하고, 그 중에서 target 수에 만족하는 개수를 반환 
+        Queue<Integer> queue = new LinkedList<>();
+        queue.add(0);
+        
+        for (int i = 0; i < numbers.length; i++) {
+            int num = numbers[i];
+            
+            ArrayList<Integer> list = new ArrayList<>();
+            while (!queue.isEmpty()) {
+                int now = queue.poll();
+                list.add(now + num);
+                list.add(now - num);
+            }
+            
+            for (Integer value : list) {
+                queue.offer(value); 
+            }
+        } 
+    
+        answer = Collections.frequency(queue, target);
         return answer;
     }
 }
