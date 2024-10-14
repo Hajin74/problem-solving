67dcaf5e135e2d7d8596ed2a7a30e8aa96119d4b
Hajin <67518596+Hajin74@users.noreply.github.com>
2024-10-14 19:50:25 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 8.29 ms, Memory: 53.5 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -1,42 +1,34 @@
 import java.util.*;
 
 class Solution {
-    public static int[] dx = {-1, 1, 0, 0};
-    public static int[] dy = {0, 0, -1, 1};
     public static int n;
     public static int m;
-    public static int[][] maps;
+    public static int[] dx = {-1, 1, 0, 0};
+    public static int[] dy = {0, 0, -1, 1};
     
     public int solution(int[][] maps) {
         this.n = maps.length;
         this.m = maps[0].length;
-        this.maps = maps;
-        
-        bfs(0, 0);
-        
-        int answer = maps[n-1][m-1];
-        
-        if (answer == 1) {
+    
+        bfs(maps, 0, 0);
+    
+        if (maps[n-1][m-1] == 1) {
             return -1;
         } else {
-            return answer;
+            return maps[n-1][m-1];
         }
     }
-    
-    public void bfs(int startX, int startY) {
-        Queue<int[]> q = new LinkedList<>();
-        q.offer(new int[]{startX, startY});
+
+    public static void bfs(int[][] maps, int x, int y) {
+        Queue<int[]> queue = new LinkedList<>();
+        queue.add(new int[]{x, y});
         
-        // 큐가 빌 때까지
-        while(!q.isEmpty()) {
-            int[] pos = q.poll();
-            int x = pos[0];
-            int y = pos[1];
+        while (!queue.isEmpty()) {
+            int[] now = queue.poll();
             
-            // 인접한 노드들 방문
             for (int i = 0; i < 4; i++) {
-                int nx = x + dx[i];
-                int ny = y + dy[i];
+                int nx = now[0] + dx[i];
+                int ny = now[1] + dy[i];
                 
                 // 범위 제한
                 if (nx < 0 || ny < 0 || nx >= n || ny >= m) {
@@ -44,8 +36,8 @@ class Solution {
                 }
                 
                 if (maps[nx][ny] == 1) {
-                    q.offer(new int[]{nx, ny});
-                    maps[nx][ny] = maps[x][y] + 1;
+                    maps[nx][ny] = maps[now[0]][now[1]] + 1; // 최단경로로 방문처리
+                    queue.add(new int[]{nx, ny});
                 }
             }
         }
