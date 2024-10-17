553e61a665d8b5d4c1f05ba4c0ee621a0d45a245
Hajin <67518596+Hajin74@users.noreply.github.com>
2024-10-17 20:00:51 +0900

    [Silver III] Title: N과 M (3), Time: 1216 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.py
@@ -0,0 +1,14 @@
+N, M = map(int, input().split())
+answer = []
+
+def backtracking():
+    if len(answer) == M:
+        print(" ".join(map(str, answer)))
+        return
+    
+    for i in range(1, N+1):
+        answer.append(i)
+        backtracking()
+        answer.pop()
+
+backtracking()
