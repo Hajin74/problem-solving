9a4c85e8b69725b9d3e94b4ca2ba08d3a8c15f0e
Hajin <67518596+Hajin74@users.noreply.github.com>
2024-10-17 20:37:23 +0900

    [Silver III] Title: N과 M (6), Time: 32 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/15655. N과 M （6）/N과 M （6）.py
@@ -0,0 +1,25 @@
+import sys
+
+N, M = map(int, input().split())
+numbers = list(map(int, sys.stdin.readline().split()))
+numbers.sort()
+
+answer = []
+
+def backtracking():
+    if len(answer) == M:
+        print(" ".join(map(str, answer)))
+        return
+    
+    for num in numbers:
+        if num not in answer:
+            if len(answer) == 0:
+                answer.append(num)
+                backtracking()
+                answer.pop()
+            elif num > answer[-1]:
+                answer.append(num)
+                backtracking()
+                answer.pop()
+
+backtracking()
