259a66eadb766e445824cbef215aec838ee4334c
Hajin <67518596+Hajin74@users.noreply.github.com>
2024-10-17 22:38:37 +0900

    [Gold V] Title: 적록색약, Time: 84 ms, Memory: 34112 KB -BaekjoonHub

--- 백준/Gold/10026. 적록색약/적록색약.py
+++ 백준/Gold/10026. 적록색약/적록색약.py
@@ -1,48 +1,56 @@
-import copy
+from collections import deque
 import sys
+import copy
 
-sys.setrecursionlimit(10**7)
+n = int(input())
+map = [list(sys.stdin.readline().rstrip()) for _ in range(n)]
 
-N = int(input())
+# 방향
+dx = [-1, 1, 0, 0]
+dy = [0, 0, -1, 1]
 
-graph = []
-for _ in range(N):
-  graph.append(list(map(str, input())))
-graph2 = copy.deepcopy(graph)
+def bfs(map, x, y):    
+    queue = deque()
+    queue.append((x, y))
+    cc = map[x][y] # 현재 컬러
 
+    while queue:
+        cx, cy = queue.popleft()
+        
+        # 인접 탐색
+        for i in range(4):
+            nx, ny = cx + dx[i], cy + dy[i]
 
-def dfs(x, y, color, graph):
-  if x < 0 or x >= N or y < 0 or y >= N:
-    return False
+            # 범위 제한
+            if nx < 0 or ny < 0 or nx >= n or ny >= n:
+                continue
+            
+            # 현재 노드와 인접 노드의 색갈이 같으면 방문 (덩어리로 묶기)
+            if map[nx][ny] != 'V' and map[nx][ny] == cc:
+                map[nx][ny] = 'V'
+                queue.append((nx, ny))
 
-  if graph[x][y] != "V" and graph[x][y] == color:
-    graph[x][y] = "V"
+non_red_green_blindness_count = 0
+non_red_green_blindness_map = copy.deepcopy(map)
+for i in range(n):
+    for j in range(n):
+        if non_red_green_blindness_map[i][j] != 'V':
+            bfs(non_red_green_blindness_map, i, j)
+            non_red_green_blindness_count += 1
+print(non_red_green_blindness_count)
 
-    dfs(x + 1, y, color, graph)
-    dfs(x - 1, y, color, graph)
-    dfs(x, y + 1, color, graph)
-    dfs(x, y - 1, color, graph)
+red_green_blindness_count = 0
+red_green_blindness_map = copy.deepcopy(map)
 
-    return True
-
-  return False
-
-
-result1 = 0
-for i in range(N):
-  for j in range(N):
-    if dfs(i, j, graph[i][j], graph):
-      result1 += 1
-
-for i in range(N):
-  for j in range(N):
-    if graph2[i][j] == "G":
-      graph2[i][j] = "R"
-
-result2 = 0
-for i in range(N):
-  for j in range(N):
-    if dfs(i, j, graph2[i][j], graph2):
-      result2 += 1
-
-print(result1, result2)
+for i in range(n):
+    for j in range(n):
+        # 빨강과 초록을 같은 색으로 만들기
+        if red_green_blindness_map[i][j] == 'R':
+            red_green_blindness_map[i][j] = 'G'
+for i in range(n):
+    for j in range(n):
+        if red_green_blindness_map[i][j] != 'V':
+            bfs(red_green_blindness_map, i, j)
+            red_green_blindness_count += 1
+       
+print(red_green_blindness_count)
