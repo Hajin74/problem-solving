437f027c98a048fd718d6746fc9b785567700717
Hajin <67518596+Hajin74@users.noreply.github.com>
2024-10-26 15:40:24 +0900

    [level 2] Title: 올바른 괄호, Time: 18.72 ms, Memory: 52.8 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -1,25 +1,29 @@
-import java.util.Stack;
+import java.util.*;
 
 class Solution {
     boolean solution(String s) {
         boolean answer = true;
 
-        Stack<String> stack = new Stack<>();
-
-        try {
-            for (int i = 0; i < s.length(); i++) {
-                char c = s.charAt(i);
-                if (c == '(') {
-                    stack.push(String.valueOf(c));
-                } else {
-                    stack.pop();
-                }
+        Stack<Character> stack = new Stack<>();
+        
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            
+            // 여는 괄호
+            if (c == '(') {
+                stack.push(c);
             }
-        } catch (Exception e) {
-            answer = false;
+            
+            // 닫는 괄호
+            if (c == ')') {
+                if (stack.size() == 0) {
+                    return false;
+                }
+                stack.pop();
+            }
         }
-
-        if (!stack.isEmpty()) {
+        
+        if (stack.size() != 0) {
             answer = false;
         }
 
