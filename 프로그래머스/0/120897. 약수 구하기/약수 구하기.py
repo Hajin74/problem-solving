206511fcca4c06ce86bd6998507e3b17b5d74a24
Hajin <67518596+Hajin74@users.noreply.github.com>
2024-11-05 17:12:18 +0900

    [level 0] Title: 약수 구하기, Time: 1.09 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/0/120897. 약수 구하기/약수 구하기.py
+++ 프로그래머스/0/120897. 약수 구하기/약수 구하기.py
@@ -1,8 +1,5 @@
 def solution(n):
-    answer = []
+
+    return [i for i in range(1, n+1) if n % i == 0]
     
-    for i in range(1, n+1):
-        if n % i == 0:
-            answer.append(i)
     
-    return answer
