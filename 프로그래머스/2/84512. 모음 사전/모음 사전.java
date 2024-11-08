a61b83abd8a47eaab3d53e076f7497a6bad2288f
Hajin <67518596+Hajin74@users.noreply.github.com>
2024-11-08 10:48:47 +0900

    [level 2] Title: 모음 사전, Time: 0.60 ms, Memory: 77.9 MB -BaekjoonHub

--- 프로그래머스/2/84512. 모음 사전/모음 사전.java
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.java
@@ -2,10 +2,17 @@ class Solution {
     static final char[] alpha =  {'A', 'E', 'I', 'O', 'U'};
     static final int[] count = {781, 156, 31, 6, 1};
     
-    // 어떤 수가 1자리에 올 때 그 뒤에 781번부터 시작이다.
-    // A_____ : 1 ~ 781, E_____ : 782 ~ 1562 
-    // 어떤 수가 2자리에 올 때 그 뒤에 156번부터 시작이다.
-    // _A___ : 3905 ~ 4060, _E___ : 4061 ~ 4215
+    // 어떤 수가 4번째 자리에 올 때, 다음의 문자가 4번째 자리에 오려면 6번이 걸린다.
+    // ___A_ : ___AA (1), ___AE (2), ___AI (3), ___AO (4), ___AU (5) , ___EA (6)
+    // 어떤 수가 5번째 자리에 올 때, 다음의 문자가 5번째 자리에 오려면 1번이 걸린다.
+    // ____E : ____I (1)
+    
+    // ex) AAAE
+    // n자리 : n-1자리의 경우 + (alpha 인덱스  * alpha 인덱스의 count) + 1
+    // 1자리 : 0 + (0 * 781) + 1 = 1
+    // 2자리 : 1 + (0 * 156) + 1 = 2
+    // 3자리 : 2 + (0 * 31) + 1 = 3
+    // 4자리 : 3 + (1 * 6) + 1 = 10
     
     public int solution(String word) {
         int sum = 0;
@@ -28,7 +35,7 @@ class Solution {
             System.out.println(sum);
         }
         
-         System.out.println(sum);
+        System.out.println(sum);
         return sum;
     }
 }
